8026d5807edb2ae04e5042fbfa0050ed6f46dd9c
meatball1982 <meatball1982@163.com>
2014-08-20 16:58:24 +0800

    add main struct

--- fun_Cluster_FP_00main.m
+++ fun_Cluster_FP_00main.m
@@ -1,8 +1,25 @@
 function [ out ] = fun_Cluster_FP_00main( X )
-%UNTITLED Summary of this function goes here
+% UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
+%% outline
+% use the science cluster algorithm to cluster the points
 
+%% 00 load data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% 01 cal the distance matrix %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[ dis_matrix ] = fun_Cluster_FP_01dismatrix( X )
+
+%% 02 cal rho %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[ rho        ] = fun_Cluster_FP_02rho( 1 )
+
+%% 03 cal deltra %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[ delta      ] = fun_Cluster_FP_03deltra( 1 )
+
+%% 04 cal halo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[ halo       ] = fun_Cluster_FP_04halo( 1 )
 
 end
+
+
 %% logs 
 % mod : 20-Aug-2014 16:11:35
