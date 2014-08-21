792416d3feb5572f00be9e0978f12db678c28b7f
meatball1982 <meatball1982@163.com>
2014-08-21 08:50:14 +0800

    21-Aug-2014 08:49:09

---
+++ work_Cluster_FP_main.m
@@ -0,0 +1,16 @@
+clear all
+clc
+
+%% outline 
+% work to cluster the ser dia with function
+% use the science cluster algorithm
+% 
+% ref : clustering by fast search and find of density peaks
+% Science 27 June 2014: 
+% Vol. 344 no. 6191 pp. 1492-1496 
+% DOI: 10.1126/science.1242072
+
+%% main
+
+%% logs 
+% mod : 21-Aug-2014 08:49:09
